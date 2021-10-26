830f574f81ac9855ae30bb76497d172de2a23503
26seung <yousi7@naver.com>
2021-10-26 23:48:52 +0900

    test

--- todolist-frontend/src/components/Main.js
+++ todolist-frontend/src/components/Main.js
@@ -4,9 +4,9 @@ import { Link } from 'react-router-dom';
 const Main = () => {
     return (
         <div>
-            main Page
+            main Page 입니다.
             <div>
-                <Link to="/todo"> 페이지 이동</Link>
+                <Link to="/todo"> 페이지 이동 변경</Link>
             </div>            
         </div>
     );
